a79119ec27a9209dc14f565eea590af0e669c500
carlos <carlos.fernandes@b2mlportal.com.br>
2022-11-11 00:27:19 -0300

    feat: First CRUD Version for car and customer (without rules yet)

--- app.py
+++ app.py
@@ -1,26 +1,103 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models import *
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 
 @app.route('/customer', methods=['GET', 'POST'])
 def customer():
-    pass
+    # TODO: remover esse create all daqui e colocar em algum outro lugar
+    db.create_all()
+    if request.method == 'GET':
+        items = [customer.__repr__() for customer in Customer.get()]
+        return jsonify(items)
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        cellphone = request.form['cellphone']
+
+        customer = Customer(name=name, email=email, cellphone_number=cellphone)
+        db.session.add(customer)
+        db.session.commit()
+        return 'sucesso'
 
 
-@app.route('/customer/<int:id>', methods=['GET', 'POST', 'DELETE'])
+@app.route('/customer/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def customer_by_id(id):
-    pass
+    if request.method == 'GET':
+        items = Customer.get(id)
+        return jsonify(items.__repr__())
+    if request.method == 'PUT':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        cellphone = request.form.get('cellphone')
+
+        customer = Customer.get(id)
+        customer.name = name if name else customer.name
+        customer.email = email if email else customer.email
+        customer.cellphone_number = cellphone if cellphone else customer.cellphone_number
+        db.session.add(customer)
+        db.session.commit()
+        return 'sucesso'
+
+    if request.method == 'DELETE':
+        customer = Customer.get(id)
+        db.session.delete(customer)
+        db.session.commit()
+
+        return 'sucesso'
+
+
+@app.route('/customer_cars/<int:id>', methods=['GET'])
+def customer_cars(id):
+    items = [car.__repr__() for car in Car.get_by_owner_id(id)]
+    return jsonify(items.__repr__())
 
 
 @app.route('/car', methods=['GET', 'POST'])
 def car():
-    pass
+    if request.method == 'GET':
+        items = [car.__repr__() for car in Car.get()]
+        return jsonify(items)
+
+    if request.method == 'POST':
+        id_owner = int(request.form['id_owner'])
+        color = request.form['color']
+        model = request.form['model']
+
+        owner = Customer.get(id_owner)
+        car = Car(owner=owner, color=color, model=model)
+        db.session.add(car)
+        db.session.commit()
+        return 'sucesso'
 
 
-@app.route('/car/<int:id>', methods=['GET', 'POST', 'DELETE'])
+@app.route('/car/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def car_by_id(id):
-    pass
+    if request.method == 'GET':
+        items = Car.get(id)
+        return jsonify(items.__repr__())
+
+    if request.method == 'PUT':
+        color = request.form['color']
+
+        car = Car.get(id)
+        car.color = color if color else car.color
+        db.session.add(car)
+        db.session.commit()
+        return 'sucesso'
+
+    if request.method == 'DELETE':
+        car = Car.get(id)
+        car.delete_car()
+        db.session.delete(car)
+        db.session.commit()
+
+        return 'sucesso'
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,11 +1,16 @@
 # from decouple import config
-from flask_sqlalchemy import SQLAlchemy, Enum
+import enum
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Enum
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 
 class Customer(db.Model):
     __tablename__ = "customer"
+    __table_args__ = {"schema": "carford"}
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
@@ -20,16 +25,43 @@ class Customer(db.Model):
         self.car_number = 0
 
     def __repr__(self):
-        return f"{self.name}:{self.email}"
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'cellphone_number': self.cellphone_number,
+            'car_number': self.car_number
+        }
+
+
+    @staticmethod
+    def get(id: int = None):
+        if id:
+            return Customer.query.filter(Customer.id == id).first()
+        return Customer.query.all()
+
+
+class EnumColor(enum.Enum):
+    yellow = "yellow"
+    blue = "blue"
+    gray = "gray"
+
+
+class EnumModel(enum.Enum):
+    hatch = "hatch"
+    sedan = "sedan"
+    convertible = "convertible"
 
 
 class Car(db.Model):
     __tablename__ = "car"
+    __table_args__ = {"schema": "carford"}
 
     id = db.Column(db.Integer, primary_key=True)
-    id_owner = db.Column(db.Integer, db.ForeignKey('customer.id'))
-    model = db.Column(db.String())
-    color = db.Column(db.String())
+    id_owner = db.Column(db.Integer, db.ForeignKey('carford.customer.id'))
+    model = db.Column(Enum(EnumModel))
+    color = db.Column(Enum(EnumColor))
+    owner = relationship('Customer')
 
     def __init__(self, owner: Customer, model, color):
         self.owner = owner
@@ -39,5 +71,23 @@ class Car(db.Model):
 
         self.owner.car_number += 1
 
+    def delete_car(self):
+        self.owner.car_number -= 1
+
     def __repr__(self):
-        return f"{self.id_owner}:{self.model} - {self.color}"
+        return {
+            'id': self.id,
+            'id_owner': self.id_owner,
+            'model': self.model.value,
+            'color': self.color.value,
+        }
+
+    @staticmethod
+    def get(id: int = None):
+        if id:
+            return Car.query.filter(Car.id == id).first()
+        return Car.query.all()
+
+    @staticmethod
+    def get_by_owner_id(id: int):
+        return Car.query.filter(Car.id_owner == id).first()
